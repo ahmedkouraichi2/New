96c992594fd273bb5955d71570fe40094d4464ff
ahmedkouraichi2 <kouraichiahmed2@gmail.com>
2023-02-27 21:42:33 +0100

    first service

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -7,12 +7,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NewsService {
    //new api url 
-
-   newsApiUrl =" https://newsapi.org/v2/everything?q=tesla&from=2023-01-27&sortBy=publishedAt&apiKey=API_KEY "
+   api_key = '1db857dd8194457aba5df231c86c533f';
+   newsApiUrl = ' https://newsapi.org/v2/everything?q=tesla&from=2023-01-27&sortBy=publishedAt&apiKey=' 
   constructor(private http :HttpClient) { }
 
   topHeading():Observable<any>
   {
-     return this.http.get(this.newsApiUrl);
+     return this.http.get(this.newsApiUrl + this.api_key);
   }
 }
